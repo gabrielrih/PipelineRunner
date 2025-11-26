bbbfce2356b43133fd662f26fcf83143b4799a98
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2025-11-25 22:45:28 -0300

    feat: adding version command

--- pipelinerunner/main.py
+++ pipelinerunner/main.py
@@ -3,9 +3,11 @@ import click
 from pipelinerunner.template.interface.cli.template_commands import template
 from pipelinerunner.runner.interface.cli.runner_commands import runner
 from pipelinerunner.runner.interface.cli.runner_run import run
+from pipelinerunner.shared.util.version import get_version, PACKAGE_NAME
 
 
 @click.group
+@click.version_option(version=get_version(), prog_name = PACKAGE_NAME)
 def main():
     ''' Manage and execute Azure DevOps pipelines in batch mode '''
     pass
